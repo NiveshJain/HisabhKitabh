584b58b6c12fe2dd946478dc550effb9dfef8d8b
Nivesh Jain <niveshjain2012@gmail.com>
2016-02-18 20:21:27 +0530

    ViewPager done+HomeActivity done

--- app/src/main/java/com/example/hisabhkitabh/HomeActivity.java
+++ app/src/main/java/com/example/hisabhkitabh/HomeActivity.java
@@ -1,17 +1,23 @@
 package com.example.hisabhkitabh;
 
-import android.annotation.TargetApi;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class HomeActivity extends AppCompatActivity{
@@ -26,27 +32,65 @@ public class HomeActivity extends AppCompatActivity{
 
     private static  int SIGN_IN_REQUEST_CODE = 0;
 
-    private static final int NUM_PAGES  = 3 ;
+    private static final int NUM_TABS = 3 ;
     private ViewPager mViewPager;
-    private PagerAdapter mPagerAdapter;
+    private FragmentSlidePagerAdapter  mPagerAdapter;
 
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        toolbar =  (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
+        toolbar =  (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mViewPager = (ViewPager) findViewById(R.id.pager);
+        setupActionBar();
 
-        mPagerAdapter = new FragmentSlidePagerAdapter(getSupportFragmentManager());
 
-        mViewPager.setAdapter(mPagerAdapter);
+
+         mViewPager = (ViewPager) findViewById(R.id.pager);
+         mPagerAdapter = new FragmentSlidePagerAdapter(getSupportFragmentManager());
+         mViewPager.setAdapter(mPagerAdapter);
+         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(setTabs()));
+
+
        }
 
+    private TabLayout setTabs() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Contancts"));
+        tabLayout.addTab(tabLayout.newTab().setText("Groups"));
+        tabLayout.addTab(tabLayout.newTab().setText("Reports"));
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        return tabLayout;
+    }
+
+
+    private void  setupActionBar (){
+
+        ActionBar actionBar =  getSupportActionBar();
+        actionBar.setCustomView(R.layout.actionbar_layout);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,7 +113,8 @@ public class HomeActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    public static class FragmentSlidePagerAdapter extends FragmentPagerAdapter {
+
+    public static class FragmentSlidePagerAdapter extends FragmentStatePagerAdapter {
 
         public FragmentSlidePagerAdapter ( FragmentManager fm){
             super(fm);
@@ -78,12 +123,80 @@ public class HomeActivity extends AppCompatActivity{
         @Override
         public Fragment getItem(int position) {
 
-            return new ContactsListFragment();
+            switch (position){
+                case 0:
+                    ContactsListFragment contactsListFragment = new ContactsListFragment();
+
+                    return contactsListFragment;
+                case 1:
+                    GroupListFragment groupListFragment = new GroupListFragment();
+                    return groupListFragment;
+                case 2:
+                    ReportFragment reportFragment = new ReportFragment();
+                    return reportFragment;
+                default:
+                    return null;
+            }
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return NUM_TABS;
+        }
+    }
+
+
+    public static class ContactsListFragment extends Fragment {
+
+        public ContactsListFragment () {}
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            View view =  inflater.inflate(R.layout.contacts_list,container,false);
+
+            return  view;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+            String [] names = {"Nivesh","Shashank","Ratnesh","Suyash","Sangeet","Yash","Sourabh","Krati","Kapil"};
+
+            ListView listview =  (ListView)  getActivity().findViewById(R.id.list);
+
+            ArrayAdapter adapter = new ArrayAdapter<String> (this.getActivity(),R.layout.contact_item_layout,R.id.name,names);
+            listview.setAdapter(adapter);
+
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                }
+            });
+        }
+    }
+
+    public  static  class GroupListFragment extends Fragment {
+
+        public GroupListFragment () {}
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.groupcontactslist_layout,container,false);
+        }
+    }
+
+    public  static  class ReportFragment extends Fragment {
+
+        public  ReportFragment () {}
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.report_layout,container,false);
         }
     }
 
